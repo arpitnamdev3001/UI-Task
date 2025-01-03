201499c5877a0bb8cbb610da0345a21d7b850be1
Arpit Namdev <162124320+arpitnamdev3001@users.noreply.github.com>
2025-01-03 19:25:19 +0530

    Update Sidebar.jsx

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,6 +1,34 @@
-import React from 'react';
+import React, { useState } from "react";
+import './Sidebar.css';
 
 const Sidebar = () => {
+
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const handleClick = (index) => {
+    setActiveIndex(index);
+  
+  };
+
+  const content = [
+    {title: "Home", icon: "bi bi-house"},
+    {title: "Connect", icon: "bi bi-people"},
+    {title: "Notification", icon: "bi bi-bell-fill"},
+    {title: "Message", icon: "bi bi-chat-dots"},
+  ];
+
+  const Settings = [
+    {title: "My Preferences"},
+    {title: "Ratings and Reviews"},
+    {title: "Account Settings"},
+    {title: "Settings"},
+    {title: "FAQ's"},
+    {title: "About Us"},
+    {title: "Help"},
+    {title: "Contact Us"},
+    {title: "Privacy Policy"},
+    ];
+
    return (
     <>
     <div
@@ -8,16 +36,26 @@ const Sidebar = () => {
       style={{ alignItems: 'center', padding: '1rem' }} 
     >
 
-      <div className="sidebar card" style={{ width: '18rem', marginBottom: '1rem' }}>
-        <ul className="list-group list-group-flush">
-          <li className="bi bi-house list-group-item" style={{ border: 'none' }}> Home</li>
-          <li className="bi bi-people list-group-item" style={{ border: 'none' }}> Connect</li>
-          <li className="bi bi-bell-fill list-group-item" style={{ border: 'none' }}> Notification</li>
-          <li className="bi bi-chat-dots list-group-item" style={{ border: 'none' }}> Message</li>
-        </ul>
+<div className="sidebar card" style={{ width: '18rem', marginBottom: '1rem' }}>
+      {content.map((text, index) => (
+        <ul  className="list-group" style={{border: "none"}}>
+        <li 
+        key={index}
+        className={`list-group-item ${activeIndex === index ? "highlighted" : ""} `}
+        onClick={() => handleClick(index)}
+        style={{ cursor: "pointer", border: "none" }}
+        > <i className={`${text.icon}`} style={{marginRight: "10px"}}></i> 
+        {text.title}
+        </li>
+      </ul>
+        
+      ))}
+        
       </div>
+
       <div className="accordion accordion-flush card" style={{ width: '18rem' }}>
-        <div className="accordion-item">
+        {Settings.map((setting, index) => (
+          <div className="accordion-item">
           <h2 className="accordion-header">
             <button
               className="accordion-button collapsed"
@@ -27,122 +65,13 @@ const Sidebar = () => {
               aria-expanded="false"
               aria-controls="flush-collapseOne"
             >
-              My Preferences
+              {setting.title}
             </button>
           </h2>
         </div>
-        <div className="accordion-item">
-          <h2 className="accordion-header">
-            <button
-              className="accordion-button collapsed"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#flush-collapseTwo"
-              aria-expanded="false"
-              aria-controls="flush-collapseTwo"
-            >
-              Ratings and Reviews
-            </button>
-          </h2>
-        </div>
-        <div className="accordion-item">
-          <h2 className="accordion-header">
-            <button
-              className="accordion-button collapsed"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#flush-collapseThree"
-              aria-expanded="false"
-              aria-controls="flush-collapseThree"
-            >
-              Account Settings
-            </button>
-          </h2>
-        </div>
-        <div className="accordion-item">
-          <h2 className="accordion-header">
-            <button
-              className="accordion-button collapsed"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#flush-collapseFour"
-              aria-expanded="false"
-              aria-controls="flush-collapseFour"
-            >
-              Settings
-            </button>
-          </h2>
-        </div>
-        <div className="accordion-item">
-          <h2 className="accordion-header">
-            <button
-              className="accordion-button collapsed"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#flush-collapseFive"
-              aria-expanded="false"
-              aria-controls="flush-collapseFive"
-            >
-              FAQ'S
-            </button>
-          </h2>
-        </div>
-        <div className="accordion-item">
-          <h2 className="accordion-header">
-            <button
-              className="accordion-button collapsed"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#flush-collapseSix"
-              aria-expanded="false"
-              aria-controls="flush-collapseSix"
-            >
-              About Us
-            </button>
-          </h2>
-        </div>
-        <div className="accordion-item">
-          <h2 className="accordion-header">
-            <button
-              className="accordion-button collapsed"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#flush-collapseSeven"
-              aria-expanded="false"
-              aria-controls="flush-collapseSeven"
-            >
-              Help
-            </button>
-          </h2>
-        </div>
-        <div className="accordion-item">
-          <h2 className="accordion-header">
-            <button
-              className="accordion-button collapsed"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#flush-collapseEight"
-              aria-expanded="false"
-              aria-controls="flush-collapseEight"
-            >
-              Contact Us
-            </button>
-          </h2>
-        </div>
-        <div className="accordion-item">
-          <h2 className="accordion-header">
-            <button
-              className="accordion-button collapsed"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#flush-collapseNine"
-              aria-expanded="false"
-              aria-controls="flush-collapseNine"
-            >
-              Privacy Policy
-            </button>
-          </h2>
-        </div>
+
+        ))}
+        
       </div>
     </div>
 </>
